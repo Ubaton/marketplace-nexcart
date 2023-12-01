7ff3ab3c4e1d764b67de1053c48ec4c7d26a7f56
Ubaton <raymondNG729@gmail.com>
2023-12-01 17:22:06 +0200

    0.4.45

--- src/content/Cart/page.jsx
+++ src/content/Cart/page.jsx
@@ -12,20 +12,20 @@ const Cart = ({ cart, onIncrease, onDecrease, onRemove }) => {
 
   return (
     <div className=" bg-object text-gray-50 rounded-3xl p-4">
-      <h2 className="text-2xl font-bold mb-4">Your Cart</h2>
+      <h2 className="text-2xl text-center font-bold mb-4">Your Cart</h2>
       {cart && cart.length > 0 ? (
         cart.map((item) => (
           <div key={item.id} className="mb-4 border-b pb-2">
-            <div className="flex flex-col p-2">
+            <div className="bg-secondary flex flex-col p-6 rounded-3xl">
               <p className="text-lg font-semibold">{item.productName}</p>
               <img
                 src={item.imageUrl}
                 alt={item.productName}
                 className="mt-2 w-full h-32 object-cover rounded-lg mb-2"
               />
-              <p className="text-gray-600">Quantity: {item.quantity}</p>
+              <p className="text-gray-50">Quantity: {item.quantity}</p>
             </div>
-            <div className="flex gap-2 mt-2 p-2">
+            <div className="bg-secondary rounded-3xl flex gap-2 mt-2 p-2">
               <button
                 onClick={() => onIncrease(item)}
                 className="bg-blue-500 text-white p-1 rounded-full"
@@ -34,13 +34,13 @@ const Cart = ({ cart, onIncrease, onDecrease, onRemove }) => {
               </button>
               <button
                 onClick={() => onDecrease(item)}
-                className="bg-red-500 text-white p-1 rounded-full"
+                className="bg-amber-500 text-white p-1 rounded-full"
               >
                 <MinusCircle />
               </button>
               <button
                 onClick={() => onRemove(item)}
-                className="bg-black text-gray-50 p-1 rounded-full"
+                className="bg-red-500 text-gray-50 p-1 rounded-full"
               >
                 <XCircle />
               </button>
